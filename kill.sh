5ab4997c2db0930d5a80854cb3a8de7622c459bf
AE <87952618+aelhadee@users.noreply.github.com>
2022-11-11 21:28:12 -0500

    Add files via upload

---
+++ kill.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+pkill -9 -f CAN_receiver.py
+pkill -9 -f CAN_sender.py
+pkill -9 -f lidar_receiver.py
+pkill -9 -f lidar_sender.py
+pkill -9 -f cam_sender.py
+pkill -9 -f cam_receiver.py
